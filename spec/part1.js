bd31ebd1b560f99e6bbbf8db5c7b0a915b9fc45f
thekiana <kianakdonaldson@gmail.com>
2019-10-22 15:43:29 -0700

    Modify tests to proper variable input

--- spec/part1.js
+++ spec/part1.js
@@ -332,7 +332,7 @@
         var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(FILL_ME_IN)).to.eql([1, 2]);
+        expect(_.uniq(numbers, true, iterator)).to.eql([1, 2]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
@@ -383,7 +383,7 @@
       });
 
       it('should apply a function to every value in an array', function() {
-        var multiplyByTwo = FILL_ME_IN;
+        var multiplyByTwo = function(x) { return x * 2 };
 
         expect(_.map([1, 2, 3], multiplyByTwo)).to.eql([2, 4, 6]);
       });
